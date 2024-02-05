a9f5a51a6d970630940ec4f124e265f91be5f81c
happydobariya29 <149614888+happydobariya29@users.noreply.github.com>
2024-02-05 17:39:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.use(cors())
+app.use(express.json());
+
+
 app.get('/',(req,res) =>{
     res.send("Succcessfull");
 });
 
-app.use(cors())
-app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
